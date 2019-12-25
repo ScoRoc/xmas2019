5d71add9885782305d87a19deac4c3061f482db2
Scott <scrosehart@gmail.com>
2019-12-25 03:03:09 -0800

    finished messages

--- src/messages/messages.js
+++ src/messages/messages.js
@@ -1,8 +1,13 @@
 export default {
-  dad: `hi dad`,
-  eva: `hi eva`,
+  dad: `Merry Christmas Dad! You have always been there for me, giving sage advice with patience and understanding.
+  I hope you know how much I appreciate and value everything you've done for me as it has allowed me to be the person I am today.
+  I'm happy we were able to see each other in November and it was awesome seeing you produce the show.
+  Can't wait to visit again. For now, hopefully this will help you kick more butt. Love you.`,
+  eva: `Merry Christmas Eva! We've had each others back and will continue to do so. It means the world to me to have someone like you in my life.
+  Someone as kind and caring as you are, someone who appreciates and understands me, someone who knows me.
+  Just as you want the best for me, I want you to succeed. Here's to looking good in 2020. Love you to the moon and back times inifity.`,
   mom: `Merry Christmas Mom! I hope you know how much I appreciate you and all you've done for me.
   You are always there for me and help me when I need it. Thank you very much.
   I wish I was there to celebrate with you but I'm glad we saw each other in November.
-  One of these times we'll have to play golf together. But in the meantime maybe you can enjoy some more golf?`,
+  One of these times we'll have to play golf together. But in the meantime maybe you can enjoy some more golf? Love you times infinity.`,
 }
