5693c80aa1776adab4049d02955590fde4cf3b18
Scott <scrosehart@gmail.com>
2019-12-25 02:48:54 -0800

    Updates

---
+++ precache-manifest.e022bd07894839e55074c5d9612c0dcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdb64fe38f809c1cb9a4ac3368f3f2d1",
+    "url": "/xmas2019/index.html"
+  },
+  {
+    "revision": "991560eb33094ebdc29e",
+    "url": "/xmas2019/static/css/main.b4cf3864.chunk.css"
+  },
+  {
+    "revision": "cb26d371f12020cca7b4",
+    "url": "/xmas2019/static/js/2.4c220dbe.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/xmas2019/static/js/2.4c220dbe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "991560eb33094ebdc29e",
+    "url": "/xmas2019/static/js/main.cc0c2e48.chunk.js"
+  },
+  {
+    "revision": "884ee0f2a3f8349edf06",
+    "url": "/xmas2019/static/js/runtime-main.d35a6178.js"
+  }
+]);
--- src/messages/messages.js
+++
@@ -1,8 +0,0 @@
-export default {
-  dad: `hi dad`,
-  eva: `hi eva`,
-  mom: `Merry Christmas Mom! I hope you know how much I appreciate you and all you've done for me.
-  You are always there for me and help me when I need it. Thank you very much.
-  I wish I was there to celebrate with you but I'm glad we saw each other in November.
-  One of these times we'll have to play golf together. But in the meantime maybe you can enjoy some more golf?`,
-}
